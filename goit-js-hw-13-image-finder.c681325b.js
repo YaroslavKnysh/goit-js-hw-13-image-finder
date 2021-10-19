fff0d70f08c2fc90ccd092028d3e99bf16a2f609
YaroslavKnysh <81957031+YaroslavKnysh@users.noreply.github.com>
2021-10-19 11:48:57 +0000

    Deploying to gh-pages from @ YaroslavKnysh/goit-js-hw-13-image-finder@3b368c2799ba1e93a7a680831d6059789b31897c 🚀

--- goit-js-hw-13-image-finder.c681325b.js
+++ goit-js-hw-13-image-finder.c681325b.js
@@ -5,4 +5,4 @@ parcelRequire=function(e,r,t,n){var i,o="function"==typeof parcelRequire&&parcel
 },{"./apiService.js":"O78K"}],"Focm":[function(require,module,exports) {
 "use strict";require("./src/sass/main.scss"),require("./src/js/main.js");
 },{"./src/sass/main.scss":"O78K","./src/js/main.js":"aRYt"}]},{},["Focm"], null)
-//# sourceMappingURL=/parcel-project-template/goit-js-hw-13-image-finder.c681325b.js.map
+//# sourceMappingURL=/goit-js-hw-13-image-finder/goit-js-hw-13-image-finder.c681325b.js.map
