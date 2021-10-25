c2e47b1d4db101bd23e10ffce8ba2f5020d61d73
YaroslavKnysh <yarik.knysh@gmail.com>
2021-10-25 14:55:46 +0300

    Обновления, стили

---
+++ src/js/apiService.js
@@ -0,0 +1,7 @@
+export default {
+  fetchPhoto: (searchQuery, pageNumber) => {
+    return fetch(
+      `https://pixabay.com/api/?image_type=photo&orientation=horizontal&q=${searchQuery}&page=${pageNumber}&per_page=12&key='23926259-20170b2e8904d12034176c2be'`,
+    );
+  },
+};
--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,19 @@
 import apiService from './apiService.js';
+import photoCardTpl from './photoCard.hbs';
+import debounce from 'lodash.debounce';
+
+const inputEl = document.querySelector('.search-form_input');
+const galleryListEl = document.querySelector('.gallery');
+const buttonEl = document.querySelector('.button');
+console.dir(inputEl);
+console.log(galleryListEl);
+console.log(buttonEl);
+
+const findPhotoFn = e => {
+  galleryListEl.innerHTML = '';
+  fetchPhoto(e.target.value, 1)
+    .then(response => response.json())
+    .then(console.log);
+};
+
+inputEl.addEventListener('input', debounce(findPhotoFn, 500));
