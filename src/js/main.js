4041c39869ddf6cfd5c9d841a965b19c130fb413
YaroslavKnysh <yarik.knysh@gmail.com>
2021-10-26 18:33:29 +0300

    Последние корректировки

--- src/js/main.js
+++ src/js/main.js
@@ -6,45 +6,48 @@ const inputEl = document.querySelector('.search-form_input');
 const galleryListEl = document.querySelector('.gallery');
 const buttonEl = document.querySelector('.button');
 
-let pageNumber = 0;
-let arrayPhotos = [];
+let pageNumber = 1;
+
 let searchValue = '';
 
+const apiServiceFn = async () => {
+  let response = await apiService(searchValue, pageNumber);
+  let photos = await response.json();
+
+  const listPhotos = photos.hits.map(photoCardTpl).join('');
+
+  galleryListEl.insertAdjacentHTML('beforeend', listPhotos);
+};
+
 const findPhotoFn = e => {
-  // localStorage.setItem('namePhoto', searchValue);
-  if (e.target.value.length != 0) {
-    arrayPhotos = [];
-    searchValue = e.target.value;
-    pageNumber = 1;
+  localStorage.setItem('namePhoto', e.target.value);
+  galleryListEl.innerHTML = '';
+  searchValue = e.target.value;
+  pageNumber = 1;
+
+  if (e.target.value.length > 0) {
+    apiServiceFn();
+    buttonEl.classList.remove('is_hidden');
+  } else {
+    buttonEl.classList.add('is_hidden');
   }
-  // else if ((e.target.value = '')) {
-  //   pageNumber = 1;
-  // }
-  else {
-    pageNumber++;
-  }
+};
+const findPhotoNextFn = e => {
+  pageNumber++;
+  apiServiceFn();
+  // const firstPhotoId = document.getElementById(photos.hits[0].id);
 
-  apiService(searchValue, pageNumber)
-    .then(response => response.json())
-    .then(photos => {
-      arrayPhotos = [...arrayPhotos, photos.hits.map(photoCardTpl).join('')];
-
-      galleryListEl.innerHTML = arrayPhotos;
-
-      const firstPhotoId = document.getElementById(photos.hits[0].id);
-
-      firstPhotoId.scrollIntoView({
-        behavior: 'smooth',
-        block: 'end',
-      });
-    });
+  // firstPhotoId.scrollIntoView({
+  //   behavior: 'smooth',
+  //   block: 'end',
+  // });
 };
 
 inputEl.addEventListener('input', debounce(findPhotoFn, 500));
-buttonEl.addEventListener('click', findPhotoFn);
+buttonEl.addEventListener('click', findPhotoNextFn);
 
-// let localStorageSave = localStorage.getItem('namePhoto');
-// if (localStorageSave !== undefined) {
-//   findPhotoFn({ target: { value: localStorageSave } });
-//   inputEl.value = localStorageSave;
-// }
+let localStorageSave = localStorage.getItem('namePhoto');
+if (localStorageSave !== undefined) {
+  findPhotoFn({ target: { value: localStorageSave } });
+  inputEl.value = localStorageSave;
+}
